8a48113bccafcce57d24cc31d9ce2fc62411cc22
Noah Kim <noahbkim@gmail.com>
2020-07-02 01:00:33 -0400

    Clean up, close to fial versioN

--- homework/hw1/decimal/solution/decimal.hpp
+++ homework/hw1/decimal/solution/decimal.hpp
@@ -4,17 +4,17 @@
 #include <climits>
 #include <cmath>
 
-typedef std::int64_t Value;
+typedef std::int64_t value_type;
 
-int8_t sign(Value value)
+inline int8_t sign(value_type value)
 {
     return (0 < value) - (value < 0);
 }
 
-constexpr Value E(size_t n)
+constexpr value_type E(size_t n)
 {
-    Value k = 1;
-    for (size_t i = 0; i < n; i++)
+    value_type k {1};
+    for (size_t i {}; i < n; i++)
     {
         k *= 10;
     }
@@ -25,39 +25,80 @@ template<size_t P>
 class Decimal
 {
 public:
-    // Default constructor creates zero
-    Decimal() : value(0) {}
-    Decimal(const Decimal<P>& decimal) : value(decimal.value) {}
-    Decimal(std::uint64_t value) : value(value) {}
-
-    // Parsing
-    Decimal(const std::string& value);
-
-    // Formatting
-    std::string to_string() const;
-
-    // Operators
-    Decimal<P> operator==(const Decimal<P>& other) const;
-    Decimal<P> operator!=(const Decimal<P>& other) const;
-    Decimal<P> operator<(const Decimal<P>& other) const;
-    Decimal<P> operator>(const Decimal<P>& other) const;
-    Decimal<P> operator+(const Decimal<P>& other) const;
-    Decimal<P> operator-(const Decimal<P>& other) const;
-    Decimal<P> operator-() const;
-    Decimal<P> operator*(const Decimal<P>& other) const;
-    Decimal<P> operator/(const Decimal<P>& other) const;
+    constexpr Decimal() : value(0) {}
+    constexpr Decimal(const Decimal& decimal) : value(decimal.value) {}
+    constexpr explicit Decimal(const std::string& value) : value(Decimal::from_string(value)) {}
 
 private:
-    Value value;
+    value_type value;
+
+    // Direct constructor
+    Decimal(value_type value) : value(value) {}
+
+    // Internal parsing
+    static int64_t from_string(const std::string& value);
+    std::string to_string() const;
+
+    // Math
+    static int64_t multiply(value_type lhs, value_type rhs);
+    static int64_t divide(value_type lhs, value_type rhs);
+
+public:
+    // Output
+    friend std::string to_string(Decimal decimal) { return decimal.to_string(); }
+    friend std::ostream& operator<<(std::ostream &os, Decimal decimal) { return os << decimal.to_string(); }
+
+    // Unary
+    friend Decimal operator+(Decimal decimal) { return Decimal(decimal.value); }
+    friend Decimal operator-(Decimal decimal) { return Decimal(-decimal.value); }
+
+    // Comparison
+    friend Decimal operator<(Decimal lhs, Decimal rhs) { return lhs.value < rhs.value; }
+    friend Decimal operator>(Decimal lhs, Decimal rhs) { return lhs.value > rhs.value; }
+    friend Decimal operator<=(Decimal lhs, Decimal rhs) { return lhs.value <= rhs.value; }
+    friend Decimal operator>=(Decimal lhs, Decimal rhs) { return lhs.value >= rhs.value; }
+    friend Decimal operator==(Decimal lhs, Decimal rhs) { return lhs.value == rhs.value; }
+    friend Decimal operator!=(Decimal lhs, Decimal rhs) { return lhs.value != rhs.value; }
+
+    // Arithmetic
+    friend Decimal operator+(Decimal lhs, Decimal rhs) { return Decimal(lhs.value + rhs.value); }
+    friend Decimal operator-(Decimal lhs, Decimal rhs) { return Decimal(lhs.value - rhs.value); }
+    friend Decimal operator*(Decimal lhs, Decimal rhs) { return Decimal(Decimal::multiply(lhs.value, rhs.value)); }
+    friend Decimal operator/(Decimal lhs, Decimal rhs) { return Decimal(Decimal::divide(lhs.value, rhs.value)); }
+    friend Decimal& operator+=(Decimal lhs, Decimal rhs) { lhs.value += rhs.value; return lhs; }
+    friend Decimal& operator-=(Decimal lhs, Decimal rhs) { lhs.value -= rhs.value; return lhs; }
+    friend Decimal& operator*=(Decimal lhs, Decimal rhs) { lhs.value = Decimal<P>::multiply(lhs.value, rhs.value); return lhs; }
+    friend Decimal& operator/=(Decimal lhs, Decimal rhs) { lhs.value = Decimal<P>::divide(lhs.value, rhs.value); return lhs; }
 };
 
-template<size_t P> Decimal<P>::Decimal(const std::string& value)
+template<size_t P>
+std::string Decimal<P>::to_string() const
+{
+    std::string result = std::to_string(this->value);
+    size_t start { this->value < 0 ? 1ul : 0ul };
+    size_t size { result.size() };
+    if (P > size - start)
+    {
+        for (size_t i {}; i < P - size + start; i++)
+        {
+            result.insert(start, "0");
+        }
+        size = P + start;
+    }
+
+    size_t radix { size - P };
+    result.insert(radix, radix == start ? "0." : ".");
+    return result;
+}
+
+template<size_t P>
+value_type Decimal<P>::from_string(const std::string& value)
 {
     std::string buffer = value;
-    std::size_t radix = value.find(".");
+    std::size_t radix { value.find(".") };
     if (radix == std::string::npos)
     {
-        this->value = stoll(buffer) * E(P);
+        return stoll(buffer) * E(P);
     }
     else
     {
@@ -65,133 +106,63 @@ template<size_t P> Decimal<P>::Decimal(const std::string& value)
         if (buffer.size() > radix + P)
         {
             buffer.erase(radix + P);
-            this->value = stoll(buffer);
+            return stoll(buffer);
         }
         else
         {
-            size_t padding = P - (buffer.size() - radix);
-            this->value = stoll(buffer) * E(padding);
+            size_t padding { P - (buffer.size() - radix) };
+            return stoll(buffer) * E(padding);
         }
     }
 }
 
-template<size_t P> std::string Decimal<P>::to_string() const
+template<size_t P>
+value_type Decimal<P>::multiply(value_type lhs, value_type rhs)
 {
-    std::string result = std::to_string(this->value);
-    size_t start = this->value < 0 ? 1 : 0;
-    size_t size = result.size();
-    if (P > size - start)
-    {
-        for (size_t i = 0; i < P - size + start; i++)
-        {
-            result.insert(start, "0");
-        }
-        size = P + start;
-    }
-
-    size_t radix = size - P;
-    result.insert(radix, radix == start ? "0." : ".");
-    return result;
-}
-
-template<size_t P> std::string to_string(const Decimal<P>& decimal)
-{
-    return decimal.to_string();
-}
-
-template<size_t P> std::ostream& operator<<(std::ostream &os, const Decimal<P>& decimal)
-{
-    os << decimal.to_string();
-    return os;
-}
-
-template<size_t P> Decimal<P> Decimal<P>::operator==(const Decimal<P>& other) const
-{
-    return this->value == other.value;
-}
-
-template<size_t P> Decimal<P> Decimal<P>::operator!=(const Decimal<P>& other) const
-{
-    return this->value != other.value;
-}
-
-template<size_t P> Decimal<P> Decimal<P>::operator<(const Decimal<P>& other) const
-{
-    return this->value < other.value;
-}
-
-template<size_t P> Decimal<P> Decimal<P>::operator>(const Decimal<P>& other) const
-{
-    return this->value > other.value;
-}
-
-template<size_t P> Decimal<P> Decimal<P>::operator+(const Decimal<P>& other) const
-{
-    return Decimal<P>(this->value + other.value);
-}
-
-template<size_t P> Decimal<P> Decimal<P>::operator-(const Decimal<P>& other) const
-{
-    return Decimal<P>(this->value - other.value);
-}
-
-template<size_t P> Decimal<P> Decimal<P>::operator-() const
-{
-    return Decimal<P>(-this->value);
-}
-
-template<size_t P> Decimal<P> Decimal<P>::operator*(const Decimal<P>& other) const
-{
-    constexpr Value E_P = E(P);
-    constexpr Value E_M = E(P - 1);
-    constexpr Value E_L = E(2 * P - 1);
+    constexpr int64_t E_P { E(P) };
+    constexpr int64_t E_PM { E(P - 1) };
 
     // Separate into integral and fractional
-    const Value this_quotient = this->value / E_P;
-    const Value this_remainder = this->value % E_P;
-    const Value other_quotient = other.value / E_P;
-    const Value other_remainder = other.value % E_P;
+    const value_type this_integral { lhs / E_P };
+    const value_type this_fractional { lhs % E_P };
+    const value_type other_integral { rhs / E_P };
+    const value_type other_fractional { rhs % E_P };
 
     // Do multiplication
-    const Value high = this_quotient * other_quotient;
-    const Value middle = this_quotient * other_remainder + this_remainder * other_quotient;
-    const Value low = this_remainder * other_remainder;
+    const value_type high { this_integral * other_integral * E_P };
+    const value_type middle { this_integral * other_fractional + this_fractional * other_integral };
 
-    // Round
-    Value rounding = middle / E_M + low / E_L;
-    if (rounding % 10 >= 5)
-    {
-        rounding += 10;
-    }
+    // Low is handled separately for rounding
+    const value_type fractional { this_fractional * other_fractional };
+    const value_type low { fractional / E_P };
+    const value_type rounding { fractional % E_P / E_PM >= 5 };
 
     // Sum
-    return Decimal<P>(high * E_P + rounding / 10);
+    return high + middle + low + rounding;
 }
 
-template<size_t P> Decimal<P> Decimal<P>::operator/(const Decimal<P>& other) const
+template<size_t P>
+value_type Decimal<P>::divide(value_type lhs, value_type rhs)
 {
-    Value left = this->value;
-    Value right = other.value;
-
-    constexpr Value E_P = E(P);
-    Value result = left / right * E_P;
-    Value remainder = left % right;
-    Value exponent = E_P;
+    constexpr int64_t E_P { E(P) };
+    value_type result { lhs / rhs * E_P };
+    value_type remainder { lhs % rhs };
+    value_type exponent { E_P };
 
     // Build the fractional part
-    for (int i = 0; i < P && remainder != 0; i++)
+    for (size_t i {}; i < P && remainder != 0; i++)
     {
         remainder *= 10;
         exponent /= 10;
-        result += remainder / right * exponent;
-        remainder %= right;
+        result += remainder / rhs * exponent;
+        remainder %= rhs;
     }
 
     // Rounding
-    if (abs(remainder * 10) / right >= 5)
+    if (abs(remainder * 10) / rhs >= 5)
     {
-        result += sign(left) * sign(right);
+        result += sign(lhs) * sign(rhs);
     }
 
-    return Decimal<P>(result);
+    return result;
 }
--- homework/hw1/decimal/solution/main.cpp
+++ homework/hw1/decimal/solution/main.cpp
@@ -5,16 +5,19 @@ using namespace std;
 
 int main()
 {
-    Decimal<6> a("3.333333335");
-    Decimal<6> b("-1231023");
+    Decimal<6> a("3.333333");
+    Decimal<6> b("7.123456");
     Decimal<6> c("4121");
-    cout << b << ", " << c << endl;
+    Decimal<6> d("5");
+//    cout << b << ", " << c << endl;
     cout << b / c << endl;
     cout << c / b << endl;
     cout << b * c << endl;
+    cout << a << " * " << b << endl;
+    cout << d * d << endl;
 
-    uint64_t x = 10000000000000;
-    Decimal<6> y("100000.00000000");
-    cout << x * x << endl;
-    cout << y * y << endl;
+//    uint64_t x = 10000000000000;
+//    Decimal<6> y("100000.00000000");
+//    cout << x * x << endl;
+//    cout << y * y << endl;
 }
